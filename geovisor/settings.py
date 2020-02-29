22938ab8c4b634546b2fe0db02efe86935f27877
Roy Yali <ryali93@gmail.com>
2020-02-29 10:28:36 -0500

    probando pasar a produccion en linux

--- geovisor/settings.py
+++ geovisor/settings.py
@@ -144,15 +144,15 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-import os
-if os.name == 'nt':
-    import platform
-    OSGEO4W = r"C:\OSGeo4W"
-    if '64' in platform.architecture()[0]:
-        OSGEO4W += "64"
-    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
-    os.environ['OSGEO4W_ROOT'] = OSGEO4W
-    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
-    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
-    GDAL_LIBRARY_PATH = r'C:\Python36\Lib\site-packages\osgeo\gdal300.dll'
-    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
+# import os
+# if os.name == 'nt':
+#     import platform
+#     OSGEO4W = r"C:\OSGeo4W"
+#     if '64' in platform.architecture()[0]:
+#         OSGEO4W += "64"
+#     assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+#     os.environ['OSGEO4W_ROOT'] = OSGEO4W
+#     os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+#     os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+#     GDAL_LIBRARY_PATH = r'C:\Python36\Lib\site-packages\osgeo\gdal300.dll'
+#     os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH']
